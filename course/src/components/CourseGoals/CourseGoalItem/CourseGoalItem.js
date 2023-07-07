ba1c71cb1792033bd23d1493873369ef6d828f00
Kunanon Srisuntiroj <son9912@gmail.com>
2023-07-07 14:31:55 +0700

    Reindenting /course

--- course/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ course/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import './CourseGoalItem.css';
 
-const CourseGoalItem = props => {
-  // const [deleteText, setDeleteText] = useState('');
+const CourseGoalItem = (props) => {
+    // const [deleteText, setDeleteText] = useState('');
 
-  const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
-    props.onDelete(props.id);
-  };
+    const deleteHandler = () => {
+        // setDeleteText('(Deleted!)');
+        props.onDelete(props.id);
+    };
 
-  return (
-    <li className="goal-item" onClick={deleteHandler}>
-      {props.children}
-    </li>
-  );
+    return (
+        <li className="goal-item" onClick={deleteHandler}>
+            {props.children}
+        </li>
+    );
 };
 
 export default CourseGoalItem;
